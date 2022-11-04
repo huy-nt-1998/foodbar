9f66e4e207ae3c0f6994eb8d5d1ac48fb2de6a90
Huy <huy.nt.it98@gmail.com>
2022-11-04 22:24:48 +0700

    Set up and prepare scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Blog from "./Pages/Blog/Blog";
+import BookATable from "./Pages/BookATable/BookATable";
+import Contact from "./Pages/Contact/Contact";
+import Menu from "./Pages/Menu/Menu";
+import './App.scss'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App container">
+    <h6>Test 1</h6>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/book-table" element={<BookATable />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </div>
 }
-
-export default App;
+export default App
