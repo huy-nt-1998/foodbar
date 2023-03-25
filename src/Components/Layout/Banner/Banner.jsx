ed5761067dec5eb669ed9f2bd0615be23de46050
Huy Nguyen Tuan <huy.nt.it98@gmail.com>
2023-03-25 10:58:39 +0700

    Fix banner

--- src/Components/Layout/Banner/Banner.jsx
+++ src/Components/Layout/Banner/Banner.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import { Link, useLocation } from "react-router-dom";
 function Banner() {
   const {state} = useLocation();
+  
+  const title = state ? state.title : "Home"
   return <div className="banner">
     <div className="banner__layout">
-      <h1 className="banner__title">{state.title}</h1>
-      {state.title === "Home" ? null : <div className="banner__link">
-        <Link className="banner__link__item"  to="/">Home</Link> {"->"} <span className="banner__link__item" >{state.title}</span>
+      <h1 className="banner__title">{title}</h1>
+      {title === "Home" ? null : <div className="banner__link">
+        <Link className="banner__link__item"  to="/">Home</Link> {"->"} <span className="banner__link__item" >{title}</span>
       </div>}
       
     </div>
